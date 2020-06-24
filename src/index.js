a0e808b47ae6c7c2b402fe308771bb00587a1de5
Landon Martin <55550837+landon-martin@users.noreply.github.com>
2020-06-24 12:19:47 -0400

    Add linting (#5)
    * Add linting
    
    * Add lint workflow
    
    * Add eslintignore

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require('@actions/core')
+const github = require('@actions/github')
 
-const getEnvUrl = require('./utils/getEnvUrl');
-const waitForHealthy = require('./utils/waitForHealthy');
+const getEnvUrl = require('./utils/getEnvUrl')
+const waitForHealthy = require('./utils/waitForHealthy')
 
-let env;
-let timeout;
+let env
+let timeout
 
 try {
-  env = core.getInput('environment') || getEnvUrl(github.context.payload.pull_request.head.ref);
-  timeout = core.getInput('timeout');
+  env = core.getInput('environment') || getEnvUrl(github.context.payload.pull_request.head.ref)
+  timeout = core.getInput('timeout')
 } catch (error) {
-  core.setFailed(error.message);
+  core.setFailed(error.message)
 }
 
 waitForHealthy(env, timeout).then((ready) => {
   if (!ready) {
-    throw Error('Environment was not ready in time');
+    throw Error('Environment was not ready in time')
   }
-  console.log('Environment is healthy!');
+  console.log('Environment is healthy!')
 }).catch((error) => {
-  core.setFailed(error.message);
-});
+  core.setFailed(error.message)
+})
--- src/utils/getEnvUrl.test.js
+++ src/utils/getEnvUrl.test.js
@@ -1,24 +1,24 @@
-const getEnvUrl = require('./getEnvUrl');
+const getEnvUrl = require('./getEnvUrl')
 
 test('Non nested JIRA branch name works', () => {
-  const branchName = 'qaa-123';
-  const url = getEnvUrl(branchName);
-  expect(url).toEqual('qaa-123.alpha.bluescape.io');
-});
+  const branchName = 'qaa-123'
+  const url = getEnvUrl(branchName)
+  expect(url).toEqual('qaa-123.alpha.bluescape.io')
+})
 
 test('Nested JIRA branch name works', () => {
-  const branchName = 'test/qaa-123';
-  const url = getEnvUrl(branchName);
-  expect(url).toEqual('qaa-123.alpha.bluescape.io');
-});
+  const branchName = 'test/qaa-123'
+  const url = getEnvUrl(branchName)
+  expect(url).toEqual('qaa-123.alpha.bluescape.io')
+})
 
 test('Middle nested JIRA branch name works', () => {
-  const branchName = 'test/qaa-123/why';
-  const url = getEnvUrl(branchName);
-  expect(url).toEqual('qaa-123.alpha.bluescape.io');
-});
+  const branchName = 'test/qaa-123/why'
+  const url = getEnvUrl(branchName)
+  expect(url).toEqual('qaa-123.alpha.bluescape.io')
+})
 
 test('No branch name throws error', () => {
-  const branchName = '';
-  expect(() => getEnvUrl(branchName)).toThrow();
-});
+  const branchName = ''
+  expect(() => getEnvUrl(branchName)).toThrow()
+})
