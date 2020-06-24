f4c87050362ddb4a548e534f944ba5e97f409948
Landon Martin <landon.martin@bluescape.com>
2020-06-24 10:40:27 -0400

    Print pull request

--- src/utils/getUrl.js
+++ src/utils/getUrl.js
@@ -1,5 +1,5 @@
 const github = require('@actions/github');
 
 module.exports = () => {
-  console.log(JSON.stringify(github.context.payload.head.ref));
+  console.log(JSON.stringify(github.context.payload.pull_request));
 };
