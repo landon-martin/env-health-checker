7819eb30f722365f2ac2d7a2b41fe2bdb879312b
Landon Martin <landon.martin@bluescape.com>
2020-06-24 10:38:51 -0400

    Print ref

--- src/utils/getUrl.js
+++ src/utils/getUrl.js
@@ -1,5 +1,5 @@
 const github = require('@actions/github');
 
 module.exports = () => {
-  console.log(JSON.stringify(github.context));
+  console.log(JSON.stringify(github.context.payload.head.ref));
 };
