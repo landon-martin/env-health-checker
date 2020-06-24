4c5a81084f393045a48f639c340a43d604763c98
Landon Martin <landon.martin@bluescape.com>
2020-06-24 11:24:58 -0400

    Fix http in url

--- src/utils/getHealthStatus.js
+++ src/utils/getHealthStatus.js
@@ -2,11 +2,11 @@ const request = require('request-promise');
 
 module.exports = async (envUrl) => {
   console.log(`Checking health for: ${envUrl}`);
-  const res = await request.get(`health.${envUrl}`, { json: true });
+  const res = await request.get(`https://health.${envUrl}`, { json: true });
 
   res.services.forEach(service => {
-    console.log(JSON.stringify(service));
     if (!service.ok) {
+      console.log(`Service was not ready: ${JSON.stringify(service)}`)
       return false
     }
   });
