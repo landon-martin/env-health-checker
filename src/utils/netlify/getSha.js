755874c6717ad7d71fe780b56ca5d68d78df33a4
Landon Martin <landon.martin@bluescape.com>
2020-07-02 14:26:26 -0400

    Cleanup logging for health status's

--- src/utils/netlify/getSha.js
+++ src/utils/netlify/getSha.js
@@ -14,6 +14,7 @@ module.exports = async (envUrl, sha) => {
   console.log(JSON.stringify(res))
 
   if (res.gitSha !== sha) {
+    console.log(`Sha doesn't match: ${res.gitSha} != ${sha}`)
     return false
   }
   return true
