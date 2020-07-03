ba865ba2d6081886dee3ee59c7b55755e90b1083
Landon Martin <landon.martin@bluescape.com>
2020-07-03 09:28:44 -0400

    Add a check for an undefined sha
    * Try to grab it from multiple places first

--- src/utils/netlify/waitForSha.js
+++ src/utils/netlify/waitForSha.js
@@ -4,6 +4,10 @@ module.exports = async (url, sha, timeout) => {
   let ready = false
   const startTime = Date.now()
 
+  if (!sha) {
+    throw Error('Sha is not defined')
+  }
+
   while (Date.now() < startTime + timeout * 60000 && !ready) {
     ready = await getSha(url, sha)
   }
